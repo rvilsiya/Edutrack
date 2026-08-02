40122effe52aac016d15d3d1c816933a0a9bb789
rvilsiya <rvilsiya@gmail.com>
2026-08-03 01:06:19 +0530

    Change CORS allowed origins to a specific value

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["8"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
