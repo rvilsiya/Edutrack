a61bce54b892aeb03520b57e720f9b43304f07fb
rvilsiya <rvilsiya@gmail.com>
2026-08-03 09:01:00 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,12 @@
 import os
+import certifi
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
 load_dotenv()
 
 MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
-client = MongoClient(MONGODB_URI)
+client = MongoClient(MONGODB_URI, tlsCAFile=certifi.where())
 
 # Connect to the edutrack database
 db = client.edutrack
